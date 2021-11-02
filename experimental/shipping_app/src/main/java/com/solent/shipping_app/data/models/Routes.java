3a8afffe1397fa0ba22965751c8e1b3dc85e22da
Ethan Moffatt <ethanmoffatt@gmail.com>
2021-11-02 15:45:30 +0000

    models

---
+++ experimental/shipping_app/src/main/java/com/solent/shipping_app/data/models/Routes.java
@@ -0,0 +1,4 @@
+package com.solent.shipping_app.data.models;
+
+public enum Routes {
+}
---
+++ experimental/shipping_app/src/main/java/com/solent/shipping_app/data/models/Ship.java
@@ -0,0 +1,114 @@
+package com.solent.shipping_app.data.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+public class Ship {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastname;
+    private String phoneNumber;
+    private String email;
+    private double salary;
+    @Enumerated(EnumType.STRING)
+    private Routes routes;
+
+    public Ship() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Routes getDepartment() {
+        return routes;
+    }
+
+    public void setDepartment(Routes department) {
+        this.routes = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", salary=" + salary +
+                ", department=" + routes +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ship ship = (Ship) o;
+        return Double.compare(ship.salary, salary) == 0
+                && Objects.equals(id, ship.id)
+                && Objects.equals(firstName, ship.firstName)
+                && Objects.equals(lastname, ship.lastname)
+                && Objects.equals(phoneNumber, ship.phoneNumber)
+                && Objects.equals(email, ship.email)
+                && routes == ship.routes;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                id, firstName, lastname, phoneNumber, email, salary, routes
+        );
+    }
+}
